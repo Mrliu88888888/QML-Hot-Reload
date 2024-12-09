fba8a8ff7b19d64265689b409c4f80cb635449c3
liumeng <liumeng1105@outlook.com>
2024-12-09 15:51:08 +0800

    <build>(cmake): 可执行文件名新增qt版本和编译器版本

--- cmake/ThirdParty.cmake
+++ cmake/ThirdParty.cmake
@@ -12,6 +12,14 @@ else()
 endif()
 if(QT)
 	message(STATUS "Using Qt version: ${QT_VERSION}")
+
+	execute_process(COMMAND qmake
+		-query QT_HOST_DATA
+		OUTPUT_VARIABLE QT_HOST_DATA
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+	get_filename_component(QT_TOOLCHAIN_NAME ${QT_HOST_DATA} NAME)
+
 	find_package(${QT} COMPONENTS Core Gui Widgets Quick REQUIRED)
 endif()
 
