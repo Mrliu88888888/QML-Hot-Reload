d5477b4ca573e1ff06adcfd74163c09f53d75c71
liumeng <liumeng1105@outlook.com>
2024-12-10 10:41:07 +0800

    <build>(script): 自动化编译脚本

--- script/build.bat
+++ script/build.bat
@@ -1,19 +1,28 @@
 @echo off
-
 chcp 65001
 
-set BUILD_TYPE="%~1"
-
-if %BUILD_TYPE%=="" (
-	set BUILD_TYPE="x64-relwithdebinfo"
-)
-
-echo BUILD_TYPE: %BUILD_TYPE%
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+call:build "Qt5.6.3-msvc2013"
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+call:build "Qt5.12.5-msvc2015_64"
+call:build "Qt5.12.5-msvc2017_64"
+call:build "Qt5.15.2-msvc2019_64"
+call:build "Qt6.5.3-msvc2019_64"
+exit 0
 
-cmake --preset %BUILD_TYPE% ..
-cd ../out/build/%BUILD_TYPE%
-cmake --build .
-ctest
-cpack
+:build
+    cmake --preset %1 .
+    cd out\build\%1
+    cmake --build .
+    cd ..\..\..
+
+    call:cp %1
+goto:eof
+
+:cp
+    cd out\build
+    mkdir bin
+    copy %1\QHotReload\*.exe bin
+    cd ..\..
+goto:eof
